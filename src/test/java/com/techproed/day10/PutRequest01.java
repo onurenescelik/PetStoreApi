ad2c2386cf5e5447319f77e903817c9f95e8fd4f
onurenescelik <onurenescelik7@gmail.com>
2021-11-24 00:39:47 +0300

    v6

--- src/test/java/com/techproed/day10/PutRequest01.java
+++ src/test/java/com/techproed/day10/PutRequest01.java
@@ -20,7 +20,7 @@ https://jsonplaceholder.typicode.com/todos/198 URL ine aşağıdaki body gönerd
       "title": "Wash the dishes",
       "completed": false
      }
-Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin
+Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin.
 {
  "userId": 21,
  "title": "Wash the dishes",
