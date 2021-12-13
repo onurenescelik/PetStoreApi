c983a887f04accfb725aae3e49d3a4de935bdd8c
onurenescelik <onurenescelik7@gmail.com>
2021-12-13 23:47:25 +0300

    v4

---
+++ src/test/java/com/techproed/day14/ReqresRequest.java
@@ -0,0 +1,18 @@
+package com.techproed.day14;
+
+import org.junit.Test;
+
+public class ReqresRequest {
+
+    @Test
+    public void test(){
+
+        ReqresToken obje=new ReqresToken();
+        System.out.println(obje.tokenAl());
+
+
+
+    }
+
+
+}
